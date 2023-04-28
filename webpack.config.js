68e1b809e539a9549ada521d2993b94428f3ceed
Nata <erbet@mail.ru>
2023-04-28 09:32:15 +0300

    1.17. Начинаем программировать

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,42 @@ module.exports  = {
   entry : './src/main.js',
 
   output: {
-    filemane: 'bundle[contenthash].js',
-    path: path.resolve(__dirname, build),
-    clean: true
+    filename: 'bundle[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
   },
 
-  devtool: '',
+  devtool: 'source-map',
 
-  plugins: [],
+  plugins: [
+    new HtmlPlugin ({
+      template: 'public/index.html',
+     }),
+
+    new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            globOptions: {
+            ignore: ['**/index.html'],
+            },
+          },
+        ]
+      }),
+],
 
   module: {
     rules: [
-
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
     ]
   }
 }
