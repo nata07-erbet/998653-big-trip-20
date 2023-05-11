329652365579d68cc174f082f5bc2638f4f3b2e3
Nata <erbet@mail.ru>
2023-05-11 20:35:19 +0300

    feat: fix after linter

--- src/view/trip-event-new.js
+++ src/view/trip-event-new.js
@@ -1,13 +1,40 @@
-return /*html*/ `<section class="trip-events">
-<h2 class="visually-hidden">Trip events</h2>
+import { createElement } from '../render.js';
+function createEventNewTemplate () {
+  return (
+    /*html*/ `<section class="trip-events">
+    <h2 class="visually-hidden">Trip events</h2>
+    <p class="trip-events__msg">Click New Event to create your first point</p>
+    <!--
+    Значение отображаемого текста зависит от выбранного фильтра:
+      * Everthing – 'Click New Event to create your first point'
+      * Past — 'There are no past events now';
+      * Present — 'There are no present events now';
+      * Future — 'There are no future events now'.
+    -->
+</section>`);
+}
 
-<p class="trip-events__msg">Click New Event to create your first point</p>
+export default class TripEventNew {
+  constructor ({point, pointDestination, pointOffers}) {
+    this.point = point;
+    this.pointDestination = pointDestination;
+    this.pointOffers = pointOffers;
+  }
 
-<!--
-  Значение отображаемого текста зависит от выбранного фильтра:
-    * Everthing – 'Click New Event to create your first point'
-    * Past — 'There are no past events now';
-    * Present — 'There are no present events now';
-    * Future — 'There are no future events now'.
--->
-</section>`
+  getTemplate() {
+    return createEventNewTemplate (this.point, this.pointDestination, this.pointOffers);
+  }
+
+  getElement() {
+    if(!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+
+
+}
