45d611c74831a516b889457bd99e0d71d1c19a8b
Nata <erbet@mail.ru>
2023-05-15 17:02:39 +0300

    feat: add offers co TripEventAddViewComponent

--- src/view/trip-event-add.js
+++ src/view/trip-event-add.js
@@ -1,5 +1,6 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import {TYPES} from '../constants/const.js';
+import {humanizePointDueDateTime} from '../utils.js';
 
 function createEventTypeTemplate(type) {
   return (
@@ -30,10 +31,34 @@ function createEventTypeName(name) {
   return (
     /*html*/`<input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${name}" list="destination-list-1">`);
 }
-function createEventAddTemplate (point, pointDestinations) {
-  const {destination, type} = point;
+
+function createDestinationList(destinations) {
+  return(
+    destinations
+      .map((destination) =>/*html*/`<option value="${destination.name}">${destination.name}</option>`)
+  ).join('');
+}
+
+function createOffersList(offers) {
+  return(
+    offers
+      .map((offer) => /*html*/ `<div class="event__offer-selector">
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked="">
+        <label class="event__offer-label" for="event-offer-luggage-1">
+          <span class="event__offer-title">${offer.title}</span>
+          +€&nbsp;
+          <span class="event__offer-price">${offer.price}</span>
+        </label>
+      </div>`)
+  );
+}
+
+function createEventAddTemplate (point, pointDestinations,pointOffers) {
+  const {basePrice, dateFrom, dateTo, destination, type} = point;
   const pointDestination = pointDestinations.find((itemDestination) => (itemDestination.id === destination));
   const {description, name, pictures} = pointDestination;
+  const pointOffersByType = pointOffers.find((pointOffer) => pointOffer.type === type).offers;
+
 
 
   return (
@@ -56,18 +81,16 @@ function createEventAddTemplate (point, pointDestinations) {
     ${createEventType(type)}
     ${createEventTypeName(name)}
     <datalist id="destination-list-1">
-    <option value="Amsterdam"></option>
-    <option value="Geneva"></option>
-    <option value="Chamonix"></option>
+    ${createDestinationList(pointDestinations)}
     </datalist>
   </div>
 
   <div class="event__field-group  event__field-group--time">
     <label class="visually-hidden" for="event-start-time-1">From</label>
-    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${humanizePointDueDateTime(dateFrom)}">
     —
     <label class="visually-hidden" for="event-end-time-1">To</label>
-   <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/ 19 00:00">
+   <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${humanizePointDueDateTime(dateTo)}">
 </div>
 
 <div class="event__field-group  event__field-group--price">
@@ -75,7 +98,7 @@ function createEventAddTemplate (point, pointDestinations) {
     <span class="visually-hidden">Price</span>
     €
   </label>
-  <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+  <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
 </div>
 
   <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -87,16 +110,7 @@ function createEventAddTemplate (point, pointDestinations) {
   <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
   <div class="event__available-offers">
-
-    <div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked="">
-      <label class="event__offer-label" for="event-offer-luggage-1">
-        <span class="event__offer-title">Add luggage</span>
-        +€&nbsp;
-        <span class="event__offer-price">30</span>
-      </label>
-    </div>
-
+  ${createOffersList(pointOffersByType)}
   </div>
 </section>
 
