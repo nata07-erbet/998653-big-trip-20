87e798fcd72e4ed97f451e0ea01e15b0d7661ca3
Nata <erbet@mail.ru>
2023-06-19 14:30:20 +0300

    feat:add trip-info-util

--- src/constants/const.js
+++ src/constants/const.js
@@ -89,4 +89,6 @@ const EditType = {
   CREATING: 'CREATING '
 };
 
-export {TYPES, CITIES, DESCRIPTION, Price, Duration, DESTINATION_COUNT, OFFER_COUNT, POINT_COUNT, FilterTypes, SortTypes,SortTypesDescription, Mode, UpdateType, UserAction, EditType};
+const DESTINATION_ITEMS_LENGTH = 3;
+
+export {TYPES, CITIES, DESCRIPTION, Price, Duration, DESTINATION_COUNT, OFFER_COUNT, POINT_COUNT, FilterTypes, SortTypes,SortTypesDescription, Mode, UpdateType, UserAction, EditType, DESTINATION_ITEMS_LENGTH};
